9eba6f128e1d212923a7c2ae929615ea06db12c4
CED-Official <ced.official@proton.me>
2025-03-13 12:20:58 +0200

    Create InputBox.gd

---
+++ Godot 4/InputBox.gd
@@ -0,0 +1,14 @@
+# Copyright (c) 2025 CED | cedtech.official@gmail.com or ced.official@proton.me
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software to deal in the Software without restriction, including
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+# 
+# See the MIT License for the full license text:
+# https://opensource.org/licenses/MIT
+
+
